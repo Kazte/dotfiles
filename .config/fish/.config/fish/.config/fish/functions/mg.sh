09622eefce2da19cbf717ebeea896ac2fa8a9148
Kazte <castellittofranco1@gmail.com>
2026-04-20 21:18:05 -0300

    first commit

---
+++ .config/fish/.config/fish/.config/fish/functions/mg.sh
@@ -0,0 +1,13 @@
+function mg
+
+    image="$1"
+
+    if [ -z "$image" ]; then
+        echo "no image provided"
+        exit 1
+    fi
+
+    matugen image "$image"
+    cp "$image" "$HOME/.current_wallpaper"
+
+end
